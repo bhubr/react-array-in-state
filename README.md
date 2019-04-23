a3d8e6a7ec7259b0a7618a21f1caed62cc2af6d3
Benoît Hubert <benoit.hubert@wildcodeschool.fr>
2019-04-23 10:52:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# React - Manage arrays in state
+
+This simple app demonstrates how to manage an array in state:
+
+* Add an item
+* Update an item
+* Remove an item
---
+++ src/ArrayInState.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class ArrayInState extends Component {
+  constructor(props) {
+    super(props);
+    // Initialize state with an empty array
+    this.state = {
+      items: []
+    };
+  }
+
+  render() {
+    return (
+      <div className="ArrayInState">
+        <ul>
+        {
+          this.state.items.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))
+        }
+        </ul>
+      </div>
+    );
+  }
+}
+export default ArrayInState;
